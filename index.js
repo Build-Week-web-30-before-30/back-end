a7b5e2d8a52f2981c183a7a526914db51154a13d
Liam Sutton <devliam90@gmail.com>
2019-11-18 12:51:43 +0000

    initial commit, install dependencies, create basic server

---
+++ index.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+
+const app = require("./api/app");
+
+const PORT = process.env.PORT || 4400;
+
+app.listen(PORT, () => {
+  console.log(`*** Server listening on ${PORT} ***`);
+});
